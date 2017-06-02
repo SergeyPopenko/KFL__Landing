ee7415828ac4deaf51e0cadac9811dbbb5bdac31
Maxim <pots1988bear@gmail.com>
2017-06-02 11:55:23 +0300

    Create styles for present block

--- src/_html_block/present/present.js
+++ src/_html_block/present/present.js
@@ -1,6 +1,9 @@
-$(".present").viewportChecker({
-  offset: "50%",
-  callbackFunction: function(elem, action){
-    elem.find(".present__img").addClass("present__img-animation");
-  }
-});
+if ($(".present").length) {
+  $(".present").viewportChecker({
+    offset: "50%",
+    callbackFunction: function(elem, action){
+      elem.find(".present__img").addClass("present__img-animation");
+    }
+  });
+}
+
