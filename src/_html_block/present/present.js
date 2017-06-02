e42f6371d41269f2509ef24d4f9da287b313227d
Maxim <pots1988bear@gmail.com>
2017-06-02 14:41:32 +0300

    New img

--- src/_html_block/present/present.js
+++ src/_html_block/present/present.js
@@ -5,5 +5,11 @@ if ($(".present").length) {
       elem.find(".present__img").addClass("present__img-animation");
     }
   });
+  if (window.innerWidth < 768) {
+    $(".present__form").viewportChecker({
+      offset: "30%",
+      classToAdd: "present__form-animation"
+    });
+  }
 }
 
