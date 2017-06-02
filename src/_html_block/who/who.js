d4b24be1c6cbc339df636f4a2110534d807fa73b
Maxim <pots1988bear@gmail.com>
2017-06-02 09:34:04 +0300

    Create mobile styles for process block

--- src/_html_block/who/who.js
+++ src/_html_block/who/who.js
@@ -1,4 +1,7 @@
-$(".who").viewportChecker({
-  classToAdd: "who--active",
-  offset: "50%"
-});
+if ($(".who").length) {
+  $(".who").viewportChecker({
+    classToAdd: "who--active",
+    offset: "50%"
+  });
+}
+
